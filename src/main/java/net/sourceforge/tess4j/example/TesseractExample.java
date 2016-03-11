0bd49da1a7f342c323afe97579bbe1a011d3e67a
Nithiyananthan G <nidhiyanandh@live.com>
2016-03-11 09:51:03 +0530

    An simple Example for OCR

---
+++ src/main/java/net/sourceforge/tess4j/example/TesseractExample.java
@@ -0,0 +1,27 @@
+package net.sourceforge.tess4j.example;
+
+import java.io.File;
+
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TesseractExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 File imageFile = new File("C:/Users/winavit01/Desktop/k2.jpg");
+	        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+	        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+	        //instance.setDatapath("C:/Program Files/Tesseract-OCR");
+	        instance.setLanguage("kan");
+	        try {
+	            Object result =instance.doOCR(imageFile);
+	           System.out.print(result);
+	        } catch (TesseractException e) {
+	            System.err.println(e.getMessage());
+	        }
+	}
+
+}
