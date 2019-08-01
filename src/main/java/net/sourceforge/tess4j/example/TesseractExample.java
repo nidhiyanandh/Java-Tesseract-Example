458d0187504e5a73959b98bf4a3056ae2d53046f
nidhiyanandh <nidhiyanandh@aol.in>
2019-08-01 19:59:12 +0530

    file path changes

--- src/main/java/net/sourceforge/tess4j/example/TesseractExample.java
+++ src/main/java/net/sourceforge/tess4j/example/TesseractExample.java
@@ -11,11 +11,11 @@ public class TesseractExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 File imageFile = new File("C:/Users/winavit01/Desktop/k2.jpg");
+		 File imageFile = new File("C:\\Users\\anand\\Desktop\\fwdofflinehtmltypingjobdemofile\\GOING TO TYPE.tif");
 	        ITesseract instance = new Tesseract();  // JNA Interface Mapping
 	        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
 	        //instance.setDatapath("C:/Program Files/Tesseract-OCR");
-	        instance.setLanguage("kan+en");
+	        instance.setLanguage("en");
 	        try {
 	            Object result =instance.doOCR(imageFile);
 	           System.out.print(result);
