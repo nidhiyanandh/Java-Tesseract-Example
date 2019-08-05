363c5a3ed070d4e2198d176fd6d5771859cf7ddb
40005560 <40005560@BLTSL01051.lnties.com>
2019-08-05 15:06:43 +0530

    ...

--- src/main/java/net/sourceforge/tess4j/example/TesseractExample.java
+++ src/main/java/net/sourceforge/tess4j/example/TesseractExample.java
@@ -11,7 +11,7 @@ public class TesseractExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 File imageFile = new File("C:\\Users\\anand\\Desktop\\fwdofflinehtmltypingjobdemofile\\GOING TO TYPE.tif");
+		 File imageFile = new File("fielpath");
 	        ITesseract instance = new Tesseract();  // JNA Interface Mapping
 	        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
 	        //instance.setDatapath("C:/Program Files/Tesseract-OCR");
